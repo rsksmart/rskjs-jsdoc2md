008655ca7958c998a271eea11d0a91c162659f03
emi <emiliorizzo@gmail.com>
2019-11-08 13:34:50 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rskjs-jsDoc2md
+
+> Jsdoc comments to markdown
---
+++ index.js
@@ -0,0 +1,14 @@
+const jsdoc2md = require('jsdoc-to-markdown')
+const defaultOptions = require('./options.json')
+
+function Jsdoc2md (options = {}) {
+  options = Object.assign(defaultOptions, options)
+  const render = (files) => {
+    const opts = Object.assign({}, options)
+    opts.files = files
+    return jsdoc2md.render(opts)
+  }
+  return Object.freeze({ render })
+}
+
+module.exports = Jsdoc2md
